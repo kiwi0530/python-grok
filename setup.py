bf078fe62c3d5034bf141a03f92f476fbb4238e2
Kiwi <kiwi0530@gmail.com>
2013-02-08 16:43:07 +0800

    Initial docs and dir structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from diskutils.core import Extension, setup
+setup(
+	name='python-grok',
+	version='0.1',
+	description="Python ctypes binding of Grok",
+	author="Kiwi",
+	author_email="kiwi0530@gmail.com",
+	url="https://github.com/kiwi0530/python-grok",
+	packages=["grok"],
+	
+	)
